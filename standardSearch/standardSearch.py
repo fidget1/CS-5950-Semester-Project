b14ce98d9967308571f05a08410c98a01a2f05af
Macallister Armstrong <macallister.armstrong@gmail.com>
2020-07-25 18:20:32 -0400

    working search interface with English lang Tweets

--- standardSearch/standardSearch.py
+++ standardSearch/standardSearch.py
@@ -1,32 +1,33 @@
-# Using venv and running app with evironment variable exports and flask command. See below.
-# https://flask.palletsprojects.com/en/1.1.x/installation/
-# https://flask.palletsprojects.com/en/1.1.x/quickstart/
+'''
+Using venv and running app with evironment variable exports and flask command. See below.
+https://flask.palletsprojects.com/en/1.1.x/installation/
+https://flask.palletsprojects.com/en/1.1.x/quickstart/
 
-# Project Setup
-# $ mkdir myproject
-# $ cd myproject
-# $ python3 -m venv venv
-# $ . venv/bin/activate
+Project Setup
+$ mkdir myproject
+$ cd myproject
+$ python3 -m venv venv
+$ . venv/bin/activate
 
-# Flask installation (in venv)
-# $ pip install Flask
+Flask installation (in venv)
+$ pip install Flask
 
-# Check version (in venv)
-# $ flask --version
-# Should be Python 3.8.2, Flask 1.1.2, Werkzeug 1.0.1
+Check version (in venv)
+$ flask --version
+Should be Python 3.8.2, Flask 1.1.2, Werkzeug 1.0.1
 
-# If using a development environment (debugging, reloading, etc.):
-# $ export FLASK_ENV=development
+If using a development environment (debugging, reloading, etc.):
+$ export FLASK_ENV=development
 
-# Running the app
-# $ export FLASK_APP=hello.py
-# $ flask run
+Running the app
+$ export FLASK_APP=hello.py
+$ flask run
 
-# Install tweepy
-# pip install tweepy
+Install tweepy
+pip install tweepy
 
-# Resource url: https://api.twitter.com/1.1/search/tweets.json
-
+Resource url: https://api.twitter.com/1.1/search/tweets.json
+'''
 from flask import Flask, render_template, request, json
 import tweepy
 import pprint
@@ -48,13 +49,15 @@ def index():
         auth.secure = True
         api = tweepy.API(auth)
         search = request.args.get("q")
+        # API.search(q[, lang][, locale][, rpp][, page][, since_id][, geocode][, show_user])
         pprint.pprint('search: ' + search)
-        public_tweets = api.search(search)
+        public_tweets = api.search(search, lang='en')
+        pprint.pprint('public_tweets:')
+        pprint.pprint(public_tweets)
         return render_template('home.html', tweets=public_tweets)
     except tweepy.TweepError as e:
-        # private tweets can be queried, but cause errors
-        # also tweets from suspended or banned users
-        # it appears the search breaks when any tweets are private
+        # These errors were caught because I was using the user_timeline API instead of api.search
+        # Keeping them just in case
         if (e.args[0] == "Not authorized."):
             pprint.pprint(e.args[0])
             return render_template('home.html')
